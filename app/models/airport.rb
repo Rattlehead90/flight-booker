844e6a648a53939f1326ec9ea8ecfa6b57c32774
Rattlehead90 <strogonov@seznam.cz>
2022-12-01 21:28:31 +0100

    Create associations between Flight and Airport

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -2,4 +2,12 @@ class Airport < ApplicationRecord
   VALID_CODE = /[A-Z]{3}/
   validates :code, presence: true, length: { is: 3 },
                    format: VALID_CODE, uniqueness: true
+
+  has_many :departure_flights, class_name: 'Flight',
+                               foreign_key: 'departure_airport_id', 
+                               dependent: :destroy
+
+  has_many :arrival_flights, class_name: 'Flight',
+                             foreign_key: 'arrival_airport_id',
+                             dependent: :destroy
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,9 @@ class Flight < ApplicationRecord
   validates :departure_airport_id, presence: true
   validates :start_datetime, presence: true
   validates :duration, presence: true
+
+  belongs_to :departure_airport, class_name: 'Airport',
+                                 foreign_key: 'departure_airport_id'
+  belongs_to :arrival_airport, class_name: 'Airport',
+                               foreign_key: 'arrival_airport_id'
 end
