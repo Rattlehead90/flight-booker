f91b603e94fd358799fb63491c715b8d2325732e
Rattlehead90 <strogonov@seznam.cz>
2022-12-02 20:30:55 +0100

    Add 4 search fields and a button

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @airports = Airport.all.map { |a| [ a.city, a.id ] }
+    @dates = Flight.all.select('DISTINCT start_datetime')
+                   .map { |f| [ f.start_datetime.strftime("%d/%m/%Y"), f.start_datetime ] }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_193744) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_180940) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
   end
 
   create_table "flights", force: :cascade do |t|
