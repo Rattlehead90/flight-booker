f6314b4070a8f7d60f162e1240c8149084cbdcf1
Rattlehead90 <strogonov@seznam.cz>
2022-12-02 21:32:17 +0100

    Change form output params to codes

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
   def index
-    @airports = Airport.all.map { |a| [ a.city, a.id ] }
+    @airports = Airport.all.map { |a| [ a.city, a.code ] }
     @dates = Flight.all.select('DISTINCT start_datetime')
                    .map { |f| [ f.start_datetime.strftime("%d/%m/%Y"), f.start_datetime ] }
   end
