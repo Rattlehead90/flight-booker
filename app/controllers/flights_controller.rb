27742db47225103b776e2ed9a087f6ff329f7e00
Rattlehead90 <strogonov@seznam.cz>
2022-12-03 20:07:07 +0100

    Add date querying

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @airports = Airport.all.map { |a| [ a.city, a.id ] }
+    @airports = Airport.all.map { |a| [a.city, a.id] }
     @dates = Flight.pluck(:start_datetime).map { |d| d.to_date }.uniq
-                   .map { |d| [ d.strftime("%d/%m/%Y"), d ] }
-    
+                   .map { |d| [d.strftime('%d/%m/%Y'), d] }
+
     @flights = Flight.where(
       departure_airport: params[:departure_airport_id],
       arrival_airport: params[:arrival_airport_id],
-      start_datetime: params[:start_datetime]
+      start_datetime: params[:start_datetime].to_date.beginning_of_day..params[:start_datetime].to_date.end_of_day
     )
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-airports = Airport.create([{ code: 'LHR', city: 'London' }, { code: 'IST', city: 'Istambul' }, 
-                           { code: 'CDG', city: 'Paris' }, { code: 'AMS', city: 'Amsterdam' }, 
+airports = Airport.create([{ code: 'LHR', city: 'London' }, { code: 'IST', city: 'Istambul' },
+                           { code: 'CDG', city: 'Paris' }, { code: 'AMS', city: 'Amsterdam' },
                            { code: 'FRA', city: 'Frankfurt am Main' }, { code: 'MAD', city: 'Madrid' },
-                           { code: 'BCN', city: 'Barcelona'}, { code: 'LGW', city: 'Gatwick' }, 
+                           { code: 'BCN', city: 'Barcelona' }, { code: 'LGW', city: 'Gatwick' },
                            { code: 'MUC', city: 'Munich' }, { code: 'FCO', city: 'Rome' }])
 
 flights_data = []
 10.times do |l|
   10.times do |i|
     10.times do |j|
-      flights_data << { departure_airport_id: i+1, arrival_airport_id: j+1,
-                  start_datetime: "2022/12/#{l+10} 10:00:00", duration: "02:10" }
-      flights_data << { departure_airport_id: j+1, arrival_airport_id: i+1,
-                  start_datetime: "2022/12/#{l+10} 20:00:00", duration: "03:10" }
+      flights_data << { departure_airport_id: i + 1, arrival_airport_id: j + 1,
+                        start_datetime: "2022/12/#{l + 10} 10:00:00", duration: '02:10' }
+      flights_data << { departure_airport_id: j + 1, arrival_airport_id: i + 1,
+                        start_datetime: "2022/12/#{l + 10} 20:00:00", duration: '03:10' }
     end
   end
 end
