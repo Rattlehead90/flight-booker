d962522c4fc8bb39818e6823a00b1089eacbdc03
Rattlehead90 <strogonov@seznam.cz>
2022-12-06 21:14:15 +0100

    Route to new bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'bookings/create'
   get 'bookings/show'
   get 'bookings/destroy'
+  resources :bookings, only: [:new, :create, :show, :destroy]
   get 'flights', to: 'flights#index'
   root 'flights#index' 
 end
