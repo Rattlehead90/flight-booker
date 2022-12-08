d9c561fc22fee6262ac13a7acf32aeab00d78fcf
Rattlehead90 <strogonov@seznam.cz>
2022-12-08 21:14:06 +0100

    Add validations to Passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,9 @@
 class Passenger < ApplicationRecord
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :passport_num, presence: true 
+  
   belongs_to :bookings
   has_many :flights, through: :bookings
 end
